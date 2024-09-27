155136269235be151f74594dc8b5d9a5bbd22db5
Kylie Roxo <kylieroxo@Kylies-MacBook-Pro.local>
2024-09-26 20:56:05 -0600

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
+// COMSC-210 | Lab 9 | Kylie Roxo
 
-int main() {
+int main () {
     // complete this code in order to make it output
-    // the numbers 1-100
-
-    for ()
+    // the numbers 1-100: compeleted for loop to have i print values 1 to 100
+    for (int i = 1; i <= 100; i++){
         cout << i << " ";
+    }
 
-    return 0;
+return 0;
 }
